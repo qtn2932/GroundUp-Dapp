768e00341f9bc1ca99f1e9cc3ba90b48faf547f2
Quan Nguyen <quan.nguyen199712@gmail.com>
2021-10-20 00:40:22 -0400

    Added test to masterchef deposit and validate correct emission

--- test/masterchef-test.js
+++ test/masterchef-test.js
@@ -24,13 +24,19 @@ beforeEach(async () => {
     qentToken.address,
     burnaddr.address,
     addr1.address,
-    100,
+    ethers.utils.parseEther("100"),
     13445720
   );
   await qentToken.transferOwnership(masterchef.address);
 });
+describe("Masterchef Property", async function () {
+  it("should have the right emission rate", async function () {
+    const qentryPerBlock = await masterchef.QentryPerBlock();
+    expect(qentryPerBlock).to.equal(ethers.utils.parseEther("100"));
+  });
+});
 
-describe("Transactional Masterchef", async function () {
+describe("Adding Pool Masterchef", async function () {
   beforeEach(async () => {
     await masterchef.addPool(100, qentToken.address, 1000, false);
   });
@@ -42,8 +48,31 @@ describe("Transactional Masterchef", async function () {
       masterchef.addPool(100, qentToken.address, 1000, false)
     ).to.be.revertedWith("nonDuplicated: duplicated");
   });
-  it("should allow deposit", async function () {
+  it("pool should have the right lp token", async function () {
     const poolInfo = await masterchef.poolInfo(0);
-    expect(poolInfo[0]).to.equal(1);
+    expect(poolInfo[0]).to.equal(qentToken.address);
+  });
+});
+
+describe("Deposit in Pool Masterchef", async function () {
+  beforeEach(async function () {
+    await masterchef.addPool(100, qentToken.address, 1000, false);
+    await qentToken.approve(masterchef.address, ethers.utils.parseEther("100"));
+    await masterchef.deposit(0, ethers.utils.parseEther("50"));
+  });
+  it("should emit deposit event once deposit", async function () {
+    await expect(masterchef.deposit(0, ethers.utils.parseEther("50")))
+      .to.emit(masterchef, "Deposit")
+      .withArgs(owner.address, 0, ethers.utils.parseEther("50"));
+  });
+  it("should show corrected deposit amount", async function () {
+    const amount = await masterchef.userInfo(0, owner.address);
+    expect(amount.amount).to.equal(ethers.utils.parseEther("45"));
+  });
+  it("should advance block", async function () {
+    const currentBlock = await ethers.provider.getBlockNumber();
+    await ethers.provider.send("evm_mine");
+    const nextBlock = await ethers.provider.getBlockNumber();
+    expect(currentBlock + 1).to.equal(nextBlock);
   });
 });
