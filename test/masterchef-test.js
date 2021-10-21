3c84dc4f38507338eba08ce453ffaf302f495e28
Quan Nguyen <quan.nguyen199712@gmail.com>
2021-10-21 00:43:03 -0400

    Added masterchef harvest test

--- test/masterchef-test.js
+++ test/masterchef-test.js
@@ -75,4 +75,36 @@ describe("Deposit in Pool Masterchef", async function () {
     const nextBlock = await ethers.provider.getBlockNumber();
     expect(currentBlock + 1).to.equal(nextBlock);
   });
+  it("should send fee to fee address", async function () {
+    const balance = await qentToken.balanceOf(addr1.address);
+    expect(balance).to.equal(ethers.utils.parseEther("5"));
+  });
+});
+
+describe("Reward and harvest Masterchef", async function () {
+  beforeEach(async function () {
+    await masterchef.addPool(100, qentToken.address, 1000, false);
+    await qentToken.approve(masterchef.address, ethers.utils.parseEther("100"));
+    await masterchef.deposit(0, ethers.utils.parseEther("50"));
+    await ethers.provider.send("evm_mine");
+    await ethers.provider.send("evm_mine");
+    await ethers.provider.send("evm_mine");
+    await ethers.provider.send("evm_mine");
+    await ethers.provider.send("evm_mine");
+  });
+  it("should have pending reward balance", async function () {
+    const pendingReward = ethers.utils.formatEther(
+      await masterchef.pendingQentry(0, owner.address)
+    );
+    expect(parseInt(pendingReward)).to.be.within(499, 500);
+  });
+  it("should allow harvest", async function () {
+    const initialBalance = await qentToken.balanceOf(owner.address);
+    await masterchef.harvest(0);
+    const afterBalance = await qentToken.balanceOf(owner.address);
+    expect(afterBalance).to.be.above(initialBalance);
+  });
+  it("should emit harvest event", async function () {
+    expect(await masterchef.harvest(0)).to.emit(masterchef, "Harvest");
+  });
 });
